bb30308bd2b97aeec0e465e879e343d1b8b71b4b
Sourcery AI <>
2022-09-19 16:29:48 +0000

    'Refactored by Sourcery'

--- tabula.py
+++ tabula.py
@@ -7,6 +7,6 @@ ascii_max = ord('~')
 d = deque(chr(x) for x in range(ascii_min, ascii_max + 1))
 
 print(''.join(d))
-for i in range(len(d)-1):
+for _ in range(len(d)-1):
     d.rotate(-1)
     print(''.join(d))
